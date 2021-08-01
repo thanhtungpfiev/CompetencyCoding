281be99e145b568869f15ca33a56f31e511fb02d
thanhtungpfiev <thanhtungpfiev@gmail.com>
2021-08-01 18:24:32 +0700

    [MSG] Add quicksort with pivot is end of array

--- Competency/SourceCode/QuickSort/main.cpp
+++ Competency/SourceCode/QuickSort/main.cpp
@@ -39,11 +39,38 @@ int partition(vector<int>& v, int lo, int hi) {
     return right;
 }
 
+int partition2(vector<int>& v, int lo, int hi) {
+    int pivot = v[hi];
+    int left = lo;
+    int right = hi - 1;
+    while (true) {
+        while (v[left] < pivot && left <= right) left++;
+        while (v[right] > pivot && left <= right) right--;
+        if (left < right) {
+            swap(v[left], v[right]);
+            left++;
+            right--;
+        } else {
+            break;
+        }
+    }
+    swap(v[left], v[hi]);
+    return left;
+}
+
 void quickSort(vector<int>& v, int lo, int hi) {
     if (lo < hi) {
         int p = partition(v, lo, hi);
         quickSort(v, lo, p - 1);
         quickSort(v, p + 1, hi);
+    }
+}
+
+void quickSort2(vector<int>& v, int lo, int hi) {
+    if (lo < hi) {
+        int p = partition2(v, lo, hi);
+        quickSort2(v, lo, p - 1);
+        quickSort2(v, p + 1, hi);
     }
 }
 
@@ -66,7 +93,7 @@ int main()
 {
     cout << "Hello World!" << endl;
     vector<int> v = {1, 3, 2, 5, 4};
-    quickSort(v, 0, v.size() - 1);
+    quickSort2(v, 0, v.size() - 1);
     printVector(v);
 
     vector<int> v1 = fillVector(100);
@@ -74,7 +101,7 @@ int main()
     printVector(v1);
 
     vector<int> v2 = {1, 3, 2, 5, 4, 7, 1 , 5};
-    quickSort(v2, 0, v2.size() - 1);
+    quickSort2(v2, 0, v2.size() - 1);
     printVector(v2);
     return 0;
 }
