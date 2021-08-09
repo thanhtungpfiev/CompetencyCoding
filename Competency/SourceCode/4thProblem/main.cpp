d1d4e66f3b710aab749a0a924c00c492616b685b
LGE\tung.dao <tung.dao@lge.com>
2021-08-09 15:05:08 +0700

    [MSG] REPROAD with stack

---
+++ Competency/SourceCode/4thProblem/main.cpp
@@ -0,0 +1,20 @@
+/**********************************************************************
+ *
+ * \author  tung.dao@lge.com
+ *
+ * \date 8/5/2021
+ *
+ * \class %{Cpp:License:ClassName}
+ *
+ * \brief
+ *
+***********************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
